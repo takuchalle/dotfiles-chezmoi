2460b9837764340379022eaacd4651ad146981b9
takuchalle <me@takuchalle.dev>
2026-02-20 11:45:53 +0900

    Add some keybinds

--- dot_config/nvim/lua/config/keybinds.lua
+++ dot_config/nvim/lua/config/keybinds.lua
@@ -1,8 +1,13 @@
-local map = vim.keymap
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' ' 
 vim.g.maplocalleader = '\\' 
 
-map.set('n', '<leader>|', ':vsplit<cr>')
-map.set('n', '<leader>-', ':split<cr>')
+map('n', '<leader>|', ':vsplit<cr>', opts)
+map('n', '<leader>-', ':split<cr>', opts)
 
+map('n', 'gh', '<C-w>h', opts)
+map('n', 'gj', '<C-w>j', opts)
+map('n', 'gk', '<C-w>k', opts)
+map('n', 'gl', '<C-w>l', opts)
